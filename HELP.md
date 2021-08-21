2b274d8c72e45e352ef57216ededf7b9da8031f0
dqcer <dqcer@sina.com>
2021-08-21 21:04:27 +0800

    动态数据源

---
+++ HELP.md
@@ -0,0 +1,3 @@
+# todo list
+
+- db集成
---
+++ dxp-integration/dxp-integration-db/src/test/java/com/dqcer/integration/db/DxpIntegrationDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dqcer.integration.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxpIntegrationDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
