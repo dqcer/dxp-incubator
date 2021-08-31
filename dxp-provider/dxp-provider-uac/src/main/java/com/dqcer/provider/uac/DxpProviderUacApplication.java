452d1b3c8167b269ae913bf00f4664708969b77f
董勤（新） <dongqing@eclincloud.com>
2021-08-31 22:28:55 +0800

    添加uac模块

---
+++ dxp-provider/dxp-provider-uac/src/main/java/com/dqcer/provider/uac/DxpProviderUacApplication.java
@@ -0,0 +1,24 @@
+package com.dqcer.provider.uac;
+
+import com.dqcer.integration.ds.EnableDynamicDataSource;
+import com.dqcer.integration.idempotent.annotation.EnableIdempotent;
+import com.dqcer.integration.log.EnableLogListener;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dongqin
+ * @description 用户认证中心应用程序
+ * @date 2021/08/31
+ */
+@EnableLogListener
+@EnableDynamicDataSource
+@EnableIdempotent
+@SpringBootApplication
+public class DxpProviderUacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DxpProviderUacApplication.class, args);
+    }
+
+}
---
+++ dxp-provider/dxp-provider-uac/src/test/java/com/dqcer/provider/uac/DxpProviderUacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dqcer.provider.uac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxpProviderUacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
