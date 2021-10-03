10a87adcc2246f22e8d2c90fdeaa0578f7ceebe8
dqcer <dqcer@sina.com>
2021-10-03 17:12:19 +0800

    解决异常

--- dxp-provider/dxp-provider-sso/src/main/java/com/dqcer/dxpprovider/sso/DxpProviderSsoApplication.java
+++ dxp-provider/dxp-provider-sso/src/main/java/com/dqcer/dxpprovider/sso/DxpProviderSsoApplication.java
@@ -7,12 +7,14 @@ import com.dqcer.integration.log.EnableSysLog;
 import com.dqcer.integration.slider.EnableSliderValid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author dongqin
  * @description sso应用程序
  * @date 2021/09/04
  */
+@EnableFeignClients(basePackages = "com")
 @EnableCache
 @EnableSliderValid
 @EnableSysLog
