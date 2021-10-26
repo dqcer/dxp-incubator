b3b47b2a724d98bf9a06862ae2bbf71eca7dba0e
董勤（新） <dongqing@eclincloud.com>
2021-10-27 00:56:28 +0800

    模板

--- dxp-framework/dxp-framework-storage/src/main/java/com/dqcer/framework/storage/CodeGenerator.java
+++ dxp-framework/dxp-framework-storage/src/main/java/com/dqcer/framework/storage/CodeGenerator.java
@@ -10,9 +10,7 @@ import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @author dongqin
@@ -46,7 +44,7 @@ public class CodeGenerator {
     public static void main(String[] args) {
 
         String moduleName = "test";
-        String tableName = "mds_user";
+        String tableName = "plt_account";
         String basePackage = "com.dqcer";
 
         // 代码生成器
@@ -62,8 +60,7 @@ public class CodeGenerator {
 
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://172.16.2.171:3308/E00001ZS?useUnicode=true&useSSL=false&characterEncoding=utf8");
-        // dsc.setSchemaName("public");
+        dsc.setUrl("jdbc:mysql://172.16.2.171:3308/E-CLOUD?useUnicode=true&useSSL=false&characterEncoding=utf8");
         dsc.setDriverName("com.mysql.cj.jdbc.Driver");
         dsc.setUsername("root");
         dsc.setPassword("123456");
@@ -72,40 +69,67 @@ public class CodeGenerator {
         // 包配置
         PackageConfig pc = new PackageConfig();
         pc.setModuleName(moduleName);
-        pc.setParent("com.dqcer");
+        pc.setParent(basePackage);
         mpg.setPackageInfo(pc);
+
+//        // 自定义配置
+//        InjectionConfig cfg = new InjectionConfig() {
+//            @Override
+//            public void initMap() {
+//                // to do nothing
+//                Map<String, Object> map = cfg.getMap();
+//                map.put("test", "terst");
+//            }
+//        };
 
         // 自定义配置
         InjectionConfig cfg = new InjectionConfig() {
             @Override
             public void initMap() {
-                // to do nothing
+                Map<String, Object> map = new HashMap<>();
+                map.put("basePackage", basePackage + "." + moduleName);
+
+                this.setMap(map);
             }
         };
 
-        // 如果模板引擎是 freemarker
-        //String templatePath = "templates/mapper.xml.ftl";
-        // 如果模板引擎是 velocity
-//         String templatePath = "/templates/mapper.xml.vm";
+
 
         // 自定义输出配置
         List<FileOutConfig> focList = new ArrayList<>();
-        // 自定义配置会被优先输出
-//        focList.add(new FileOutConfig(templatePath) {
-//            @Override
-//            public String outputFile(TableInfo tableInfo) {
-//                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-//                return projectPath + "/src/main/java/" + basePackage.replaceAll("\\.", "\\/") + "/" + pc.getModuleName()
-//                        + "/" + tableInfo.getEntityName() + "Entity.hbm" + StringPool.DOT_XML;
-//            }
-//        });
-
+        // biz
         focList.add(new FileOutConfig("/templates/biz.java.vm") {
             @Override
             public String outputFile(TableInfo tableInfo) {
                 // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
                 return projectPath + "/src/main/java/" + basePackage.replaceAll("\\.", "\\/") + "/" + pc.getModuleName()
-                        + "/" + tableInfo.getEntityName() + "Biz" + StringPool.DOT_JAVA;
+                        + "/biz/" + tableInfo.getEntityName() + "Biz" + StringPool.DOT_JAVA;
+            }
+        });
+        focList.add(new FileOutConfig("/templates/bizImpl.java.vm") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/java/" + basePackage.replaceAll("\\.", "\\/") + "/" + pc.getModuleName()
+                        + "/biz/impl/" + tableInfo.getEntityName() + "BizImpl" + StringPool.DOT_JAVA;
+            }
+        });
+        // api
+        focList.add(new FileOutConfig("/templates/entity.java.vm") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/java/" + basePackage.replaceAll("\\.", "\\/") + "/" + pc.getModuleName()
+                        + "/api/entity/" + tableInfo.getEntityName() + "Entity" + StringPool.DOT_JAVA;
+            }
+        });
+
+        focList.add(new FileOutConfig("/templates/hbm.xml.vm") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/java/" + basePackage.replaceAll("\\.", "\\/") + "/" + pc.getModuleName()
+                        + "/api/entity/" + tableInfo.getEntityName() + ".hbm." + StringPool.DOT_XML;
             }
         });
 
@@ -121,7 +145,10 @@ public class CodeGenerator {
         // templateConfig.setService();
         // templateConfig.setController();
 
-        templateConfig.setXml("/templates/mapper.xml.vm");
+        templateConfig.setXml("/templates/hbm.xml.vm");
+//        templateConfig.setMapper("/templates/biz.java");
+        templateConfig.setMapper(null);
+        templateConfig.setEntity(null);
         mpg.setTemplate(templateConfig);
 
         // 策略配置
@@ -140,6 +167,7 @@ public class CodeGenerator {
         strategy.setTablePrefix(pc.getModuleName() + "_");
 
         gc.setXmlName("%sEntity.hbm");
+
 
         mpg.setStrategy(strategy);
         mpg.setTemplateEngine(new VelocityTemplateEngine());
